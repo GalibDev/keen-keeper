9691c420b73ba5bcce42d6ff9fe851ad3f85db08
GalibDev <mirza.galib.palash>
2026-04-17 19:51:34 +0600

    fix routing for GitHub Pages using HashRouter and base config

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import Home from "./pages/Home";
+import Timeline from "./pages/Timeline";
+import Stats from "./pages/Stats";
+import FriendDetails from "./pages/FriendDetails";
+import NotFound from "./pages/NotFound";
+
 const App = () => {
-  return null;
+  return (
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path="timeline" element={<Timeline />} />
+        <Route path="stats" element={<Stats />} />
+        <Route path="friend/:id" element={<FriendDetails />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-import router from "./routes/Routes";
+import App from "./App";
 import { TimelineProvider } from "./contexts/TimelineContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <TimelineProvider>
-      <RouterProvider router={router} />
+      <HashRouter>
+        <App />
+      </HashRouter>
       <Toaster position="top-right" />
     </TimelineProvider>
   </React.StrictMode>
